5317fe99e04f65efd37433d5bbdb43942562ecad
Jaxius6 <jetjack6@gmail.com>
2024-11-29 21:34:51 +0800

    Update next.config.js and package dependencies

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  images: {
+    unoptimized: true,
+    domains: ['localhost'],
+  },
+  reactStrictMode: true,
+};
 
 module.exports = nextConfig;

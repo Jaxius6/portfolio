9ac73dc61f681c48c4cab4d99832211a3d9e293d
Jaxius6 <jetjack6@gmail.com>
2024-11-29 17:45:46 +0800

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         >
           <ThemeToggle />
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>

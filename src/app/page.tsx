7d16321fd463ee93eafeb1171c071440fff45470
Jaxius6 <jetjack6@gmail.com>
2024-11-30 13:18:00 +0800

    feat: add Lottie animation to homepage
    - Add circle animation JSON
    - Implement Lottie with SSR-safe dynamic import
    - Position animation behind title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,22 @@
 
 import Link from "next/link";
 import "./animations.css";
+import dynamic from 'next/dynamic';
+import circleAnimation from "../../public/images/circle.json";
+
+const Lottie = dynamic(() => import('lottie-react'), { ssr: false });
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen gap-16 px-4">
-      <h1 className="title">
-        Jack Taylor
-      </h1>
+      <div className="relative">
+        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-64 h-64">
+          <Lottie animationData={circleAnimation} loop={true} />
+        </div>
+        <h1 className="title relative z-10">
+          Jack Taylor
+        </h1>
+      </div>
       
       <nav className="flex flex-col md:flex-row gap-6 md:gap-12 text-center md:text-left">
         <Link href="/code" className="nav-link">
